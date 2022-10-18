94d7fb8945199bce54901e5179a920cb1c6aefae
dakoo096 <noelia_cabral96@hotmail.com>
2022-10-17 23:58:05 -0300

    se arma tienda remera

--- frontend/teamfireAngular/src/app/app-routing.module.ts
+++ frontend/teamfireAngular/src/app/app-routing.module.ts
@@ -5,12 +5,18 @@ import { PrincipalComponent } from './componentes/paginas/principal/principal.co
 import { RegistroComponent } from './componentes/paginas/registro/registro.component';
 import { LoginComponent } from './componentes/paginas/login/login.component';
 import { TiendaprincipalComponent } from './componentes/paginas/tiendaprincipal/tiendaprincipal.component';
+import { TiendallaverosComponent } from './componentes/paginas/tiendallaveros/tiendallaveros.component';
+import { TiendaremerasComponent } from './componentes/paginas/tiendaremeras/tiendaremeras.component';
+import { TiendatazasComponent } from './componentes/paginas/tiendatazas/tiendatazas.component';
 
 const routes: Routes = [
   {path: 'principal', component: PrincipalComponent},
   {path: 'registro', component: RegistroComponent},
   {path:'login',component:LoginComponent},
-  {path:'tiendaprincipal', component:TiendaprincipalComponent}
+  {path:'tiendaprincipal', component:TiendaprincipalComponent},
+  {path:'tiendallaveros',component:TiendallaverosComponent},
+  {path:'tiendaremeras',component:TiendaremerasComponent},
+  {path:'tiendatazas',component:TiendatazasComponent}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
